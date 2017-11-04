2a873e5311b17127576cfdf4ef59b6ff5d79b6a5
cbcoutinho <chrisbcoutinho@gmail.com>
2017-11-04 16:26:21 +0100

    Fix indentation through parindent/atom

--- seesaw-test/src/seesaw_test/core.clj
+++ seesaw-test/src/seesaw_test/core.clj
@@ -1,16 +1,16 @@
 (ns seesaw-test.core
   (:use seesaw.core))
 
-  (defn -main
-    "The main function"
-    [& args]
-    (defn handler
-      [event]
-      (alert event
-        (str "<html>Hello from <b>Clojure</b>. Button "
-        (.getActionCommand event) " clicked.")))
+(defn -main
+  "The main function"
+  [& args]
+  (defn handler
+    [event]
+    (alert event
+      (str "<html>Hello from <b>Clojure</b>. Button "
+       (.getActionCommand event) " clicked.")))
 
-        (-> (frame :title "Hello Swing" :on-close :exit
-        :content (button :text "Click Me" :listen [:action handler]))
-        pack!
-        show!))
+  (-> (frame :title "Hello Swing" :on-close :exit
+       :content (button :text "Click Me" :listen [:action handler]))
+   pack!
+   show!))
