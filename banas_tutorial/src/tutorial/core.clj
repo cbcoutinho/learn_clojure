74fe5f47960c2e39c57c86c6413633e910bdc385
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 00:44:35 +0100

    Initial commit

---
+++ banas_tutorial/src/tutorial/core.clj
@@ -0,0 +1,16 @@
+(ns tutorial.core
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (def str1 "This is my 2nd String")
+  (str/blank? str1)
+  (str/includes? str1 "second")
+  (str/split str1 #" ")
+  (str/split str1 #"\d")
+  (str/replace "I am 42" #"42" "43")
+  (str/upper-case str1)
+  (str/lower-case str1))
+
