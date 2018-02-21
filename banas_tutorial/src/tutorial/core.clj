f16efb1a6760822d3e95a67255c88f478cdc5691
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 00:54:04 +0100

    Add some list-related lines

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -12,5 +12,8 @@
   (str/split str1 #"\d")
   (str/replace "I am 42" #"42" "43")
   (str/upper-case str1)
-  (str/lower-case str1))
+  (str/lower-case str1)
+
+  (println (list "Dog" 1 3.41 true))
+  (println (first (list 1 2 3))))
 
