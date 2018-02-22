9468281d06158c1b47ce43855dc3baf9a82f73d4
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 12:35:41 +0100

    Finish `struct-map`s

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -5,8 +5,18 @@
 (use 'clojure.java.io)
 
 ; Struct maps
+(defn struct-map-ex
+  []
+  (defstruct Customer :Name :Phone)
+  (def cust1 (struct Customer "Chris" 1234567))
+  (def cust2 (struct-map Customer :Name "Sally"
+                                  :Phone 1234567))
+  (println cust1)
+  (println cust2)
+  (println (format "Cust1 :Name is %s" (:Name cust1))))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args])
+  [& args]
+  (struct-map-ex))
