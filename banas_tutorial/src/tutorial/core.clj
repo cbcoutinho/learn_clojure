47b66f0b10c677a81fcd351ef5494f756cfaa837
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 10:39:23 +0100

    Made all different types of functions - one also with an interface

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -2,37 +2,38 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-; Math
-(defn math-stuff
-  []
-  (println (+ 1 2 3))          ;; Add values together
-  (println (- 5 3 2))          ;; Subtract values
-  (println (* 2 5))            ;; Multiply Values
-  (println (/ 10 5))           ;; Divide Values
-  (println (mod 12 5))         ;; Modulus
-  (println (inc 5))            ;; Increment
-  (println (dec 5))            ;; Decrement
+(defn say-hello
+  "Receives a name with 1 parameter and responds with 'Hello'"
+  [name]
+  (println "Hello again " name))
 
-  (println (Math/abs -10))     ;; Absolute Value
-  (println (Math/cbrt 8))      ;; Cube Root
-  (println (Math/sqrt 4))      ;; Square Root
-  (println (Math/ceil 4.5))    ;; Round up
-  (println (Math/floor 4.5))   ;; Round down
-  (println (Math/exp 1))       ;; e to the power of 1
-  (println (Math/hypot 2 2))   ;; sqrt(x^2 + y^2)
-  (println (Math/log 2.71828)) ;; Natural logarithm
-  (println (Math/log10 100))   ;; Base 10 log
-  (println (Math/max 1 5))
-  (println (Math/min 1 5))
-  (println (Math/pow 2 2))     ;; Power
+; Adds two numbers together
+(defn get-sum
+  [x y]
+  (println (+ x y)))
 
-  (println (rand-int 20))      ;; Generate random int
+; Uses an interface to add either two or three numbers together
+(defn get-sum-more
+  ([x y z]
+   (println (+ x y z)))
+  ([x y]
+   (get-sum x y))) ; `println` is already implemented in get-sum
 
-  (println (reduce + [1 2 3])) ;; Use reduce on a vector
-  (println Math/PI))           ;; Math has Pi built in
+; Creates a "Hello <name>" String
+(defn hello-you
+  [name]
+  (str "Hello " name))
+
+; Says hello to each name in a list
+(defn hello-all
+  [& names]
+  (println (map hello-you names)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (math-stuff))
-
+  (say-hello "Chris")
+  (get-sum 4 5)
+  (get-sum-more 3 4 5)
+  (get-sum-more 3 4)
+  (hello-all "Doug" "Mary" "Paul"))
