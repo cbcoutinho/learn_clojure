8febcbbd56625243b3dd0054f07ddcabac7fddc5
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 01:02:38 +0100

    Added sets

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -5,6 +5,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+
+  ; Strings
   (def str1 "This is my 2nd String")
   (str/blank? str1)
   (str/includes? str1 "second")
@@ -14,6 +16,13 @@
   (str/upper-case str1)
   (str/lower-case str1)
 
+  ; Lists
   (println (list "Dog" 1 3.41 true))
   (println (nth (list 1 2 3) 0))
-  (println (cons 3 (list 1 2))))
+  (println (cons 3 (list 1 2)))
+
+  ; Sets
+  (println (get (set '(1 1 2)) 2))
+  (println (get (set '(3 2)) 2))
+  (println (contains? (set '(1 2 3)) 4))
+  (println (disj (set '(1 2 3)) 3)))
