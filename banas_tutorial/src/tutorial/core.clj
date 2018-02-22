8fd437c0984bce03eeba84b60dc9733e7aabe1a7
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 02:14:50 +0100

    Add math functions

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -2,23 +2,32 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-; Agents
-(defn agent-ex
+; Math
+(defn math-stuff
   []
+  (println (+ 1 2 3))          ;; Add values together
+  (println (- 5 3 2))          ;; Subtract values
+  (println (* 2 5))            ;; Multiply Values
+  (println (/ 10 5))           ;; Divide Values
+  (println (mod 12 5))         ;; Modulus
+  (println (inc 5))            ;; Increment
+  (println (dec 5))            ;; Decrement
 
-  (def tickets-sold (agent 0))
-  (send tickets-sold + 15)
-  (println)
-  (println "Tickets " @tickets-sold))
-
-  ;(send tickets-sold + 10)
-  ;(await-for 100 tickets-sold)
-  ;(println "Tickets " @tickets-sold)
-
-  ;(shutdown-agents))
+  (println (Math/abs -10))     ;; Absolute Value
+  (println (Math/cbrt 8))      ;; Cube Root
+  (println (Math/sqrt 4))      ;; Square Root
+  (println (Math/ceil 4.5))    ;; Round up
+  (println (Math/floor 4.5))   ;; Round down
+  (println (Math/exp 1))       ;; e to the power of 1
+  (println (Math/hypot 2 2))   ;; sqrt(x^2 + y^2)
+  (println (Math/log 2.71828)) ;; Natural logarithm
+  (println (Math/log10 100))   ;; Base 10 log
+  (println (Math/max 1 5))
+  (println (Math/min 1 5))
+  (println (Math/pow 2 2)))    ;; Power
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (agent-ex))
+  (math-stuff))
 
