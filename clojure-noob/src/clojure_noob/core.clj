98c6ba1d976f251571c35b3d4e0e24f53f4d929f
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 15:04:17 +0100

    Create some factorial functions for longs and BigInts

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,31 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn factorial [n]
+  (if (= n 0) 1
+      (* n (factorial (dec n)))))
+
+(defn factorial-bigint [n]
+  (if (= n 0) 1
+      ; the *'-prefix returns arbitrary precision numbers
+      ; (clojure.lang.BigInt)
+      (*' n (factorial-bigint (dec n)))))
+
+; This will fail because recur is not in tail position
+;(defn factorial-bigint-recur [n]
+  ;(if (= n 0) 1
+      ;(* n (recur (dec n)))))
+
+(defn factorial-bigint-loop [n]
+  (if (= n 0) 1
+      (loop [val n i n]
+        (if (<= i 1) val
+          (recur (*' val (dec i)) (dec i))))))
+
 (defn -main
   "I don't do a whole lot ... yet. But I will!"
   [& args]
-  (println "Hello, World!"))
+  (factorial 25)
+  (factorial-bigint 25)
+  ;(factorial-bigint-recur 25000)
+  (factorial-bigint-loop 2500))
