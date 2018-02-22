8b660af83ab3c35fe5bdaf60cdb11d09a75cabf1
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 01:20:58 +0100

    Added some maps and map-related operations

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -30,4 +30,18 @@
   ; Vectors
   (println (get (vector 3 2 1) 2))
   (println (pop (vector 1 2 3 4)))
-  (println (subvec (vector 1 2 3 4) 1 3)))
+  (println (subvec (vector 1 2 3 4) 1 3))
+
+  ; Maps
+  (def my-map (hash-map "Name" "Chris" "Age" 26))
+  (println my-map)
+  (println (sorted-map 2 "B" 1 "A" 4 "D" 3 "C"))
+  (println (get my-map "Name")) ; Only returns value
+  (println (apply hash-map (find my-map "Name"))) ; Returns both key and value
+  (println (contains? my-map "Name"))
+  (println (keys my-map))
+  (println (vals my-map))
+  (def my-map2 (merge-with + 
+                       (apply hash-map (find my-map "Name")) 
+                       (apply hash-map (find my-map "Age"))))
+  (println (= my-map my-map2)))
