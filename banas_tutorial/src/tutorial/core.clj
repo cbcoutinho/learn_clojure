d00006d76e425fe88c92bc6352647a9276aa1cb5
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 02:02:33 +0100

    Work with agents
    The commented lines will create a threading error when uncommented. I
    can't figure out what's wrong yet

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -2,26 +2,23 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-; Atoms
-(defn atom-ex
-  [x]
+; Agents
+(defn agent-ex
+  []
 
-  (def atomEx (atom x))
-  
-  (add-watch atomEx :watcher
-       (fn [key atom old-state new-state]
-         (println "atomEx changed from "
-                  old-state " to " new-state)))
+  (def tickets-sold (agent 0))
+  (send tickets-sold + 15)
+  (println)
+  (println "Tickets " @tickets-sold))
 
-  (println "1st x: " @atomEx)
-  (reset! atomEx 10)
-  (println "2nd x: " @atomEx)
-  (swap! atomEx inc)
-  (println "3rd x: " @atomEx))
+  ;(send tickets-sold + 10)
+  ;(await-for 100 tickets-sold)
+  ;(println "Tickets " @tickets-sold)
 
+  ;(shutdown-agents))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (atom-ex 5))
+  (agent-ex))
 
