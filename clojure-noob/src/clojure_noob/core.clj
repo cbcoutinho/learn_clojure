9a48276e262aefba078b4112fba18bfa98cf0b7e
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 18:54:03 +0100

    Add various implementations of fizzbuzz from around the wild

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,58 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defmacro check-mod
+  "This macro returns a function which calculates (= (mod _ x) 0)"
+  [x]
+  (fn [i] (zero? (mod i x))))
+
+; Four fizzbuzz implementations from here:
+; http://eddmann.com/posts/fizzbuzz-in-clojure/
+
+; Original function definitions included anonymous functions, I
+; switched those to using a macro
+;(def fizz? #(zero? (mod % 3)))
+(def fizz? (check-mod 3))
+(def buzz? (check-mod 5))
+(def fizzbuzz? (check-mod 15))
+
+(def fizzbuzz1
+  "Returns an anonymous function that contains a conditional"
+  #(cond
+     (fizzbuzz? %) "FizzBuzz"
+     (fizz? %) "Fizz"
+     (buzz? %) "Buzz"
+     :else %))
+
+(defn fizzbuzz2 [n]
+  "This function builds a string. If empty, returns a number"
+  (let [s (str (if (fizz? n) "Fizz") (if (buzz? n) "Buzz"))]
+    (if (empty? s) n s)))
+
+
+(defn fizzbuzz3 [n]
+  "Similar to the 'cond' approach in fizzbuzz1"
+  (let [to-words (some-fn #(when (fizzbuzz? %) "FizzBuzz")
+                          #(when (fizz? %) "Fizz")
+                          #(when (buzz? %) "Buzz"))]
+    (or (to-words n) n)))
+
+(defn fizzbuzz4
+  ([n] (fizzbuzz4 n (array-map fizz? "Fizz" buzz? "Buzz")))
+  ([n lookup] (if-let [matches (seq (keep (fn [[pred? word]] (when (pred? n) word)) lookup))]
+                (apply str matches)
+                n)))
+
+; Simple patten matching using a single map lookup
+; https://gist.github.com/walkermatt/08e971e28eb023a128ca
+; Simple patten matching using a single map lookup
+(defn fizzbuzz5 [x]
+  (let [v [(= (mod x 3) 0) (= (mod x 5) 0)]]
+    ({[true false] "Fizz"
+      [false true] "Buzz"
+      [true true]  "FizzBuzz"
+      [false false] x} v)))
 
 (defn -main
-  "I don't do a whole lot ... yet. But I will!"
+  "This code implements fizz-buzz in various ways"
   [& args])
--- clojure-noob/test/clojure_noob/core_test.clj
+++ clojure-noob/test/clojure_noob/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;(deftest a-test
+  ;(testing "FIXME, I fail."
+    ;(is (= 0 1))))
+
+(def test-string '(1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz", 37, 38, "Fizz", "Buzz", 41, "Fizz", 43, 44, "FizzBuzz", 46, 47, "Fizz", 49, "Buzz", "Fizz", 52, 53, "Fizz", "Buzz", 56, "Fizz", 58, 59, "FizzBuzz", 61, 62, "Fizz", 64, "Buzz", "Fizz", 67, 68, "Fizz", "Buzz", 71, "Fizz", 73, 74, "FizzBuzz", 76, 77, "Fizz", 79, "Buzz", "Fizz", 82, 83, "Fizz", "Buzz", 86, "Fizz", 88, 89, "FizzBuzz", 91, 92, "Fizz", 94, "Buzz", "Fizz", 97, 98, "Fizz", "Buzz"))
+
+(deftest fizzbuzz1-test
+  (testing "fizzbuzz1"
+    (is (= (map fizzbuzz1 (range 1 101)) test-string))))
+
+(deftest fizzbuzz2-test
+  (testing "fizzbuzz2"
+    (is (= (map fizzbuzz2 (range 1 101)) test-string))))
+
+(deftest fizzbuzz3-test
+  (testing "fizzbuzz3"
+    (is (= (map fizzbuzz3 (range 1 101)) test-string))))
+
+(deftest fizzbuzz4-test
+  (testing "fizzbuzz4"
+    (is (= (map fizzbuzz4 (range 1 101)) test-string))))
+
+(deftest fizzbuzz5-test
+  (testing "fizzbuzz5"
+    (is (= (map fizzbuzz5 (range 1 101)) test-string))))
+
