5c76c5230d55341c2fc9366d392ee4784c2230d3
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 13:07:41 +0100

    Add some advanced functions

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -5,6 +5,11 @@
 (use 'clojure.java.io)
 
 ; Advanced functions
+(take 2 [1 2 3])
+(drop 1 [1 2 3])
+(take-while neg? [-1 0 1])
+(drop-while neg? [-1 0 1])
+(filter #(> % 2) [1 2 3 4])
 
 
 (defn -main
