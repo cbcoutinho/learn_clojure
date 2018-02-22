3dc23a11f04ead6ab3be749edd0605ef9c542369
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 12:27:08 +0100

    Finish File I/O

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -3,8 +3,35 @@
   (:gen-class))
 
 ; File I/O
+(use `clojure.java.io)
 
+(defn write-to-file
+  [file text]
+  (with-open [wrtr (writer file)]
+    (.write wrtr text)))
+
+(defn read-from-file
+  [file]
+  (try
+    (println (slurp file))
+    (catch Exception e
+      (println "Error : " (.getMessage e)))))
+
+(defn append-to-file
+  [file text]
+  (with-open [wrtr (writer file :append true)]
+    (.write wrtr text)))
+
+(defn read-line-from-file
+  [file]
+  (with-open [rdr (reader file)]
+    (doseq [line (line-seq rdr)]
+      (println line))))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args])
+  [& args]
+  (write-to-file "test.txt" "This is a sentence\nThis is a second line\n")
+  (read-from-file "test.txt")
+  (append-to-file "test.txt" "This is another line\n")                  
+  (read-line-from-file "test.txt"))
