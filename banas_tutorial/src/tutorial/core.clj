6ce4e394479224bd188d2b9ab61201369a686173
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 01:31:17 +0100

    Use atoms to mutate data

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -2,46 +2,26 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
+; Atoms
+(defn atom-ex
+  [x]
+
+  (def atomEx (atom x))
+  
+  (add-watch atomEx :watcher
+       (fn [key atom old-state new-state]
+         (println "atomEx changed from "
+                  old-state " to " new-state)))
+
+  (println "1st x: " @atomEx)
+  (reset! atomEx 10)
+  (println "2nd x: " @atomEx)
+  (swap! atomEx inc)
+  (println "3rd x: " @atomEx))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (atom-ex 5))
 
-  ; Strings
-  (def str1 "This is my 2nd String")
-  (str/blank? str1)
-  (str/includes? str1 "second")
-  (str/split str1 #" ")
-  (str/split str1 #"\d")
-  (str/replace "I am 42" #"42" "43")
-  (str/upper-case str1)
-  (str/lower-case str1)
-
-  ; Lists
-  (println (list "Dog" 1 3.41 true))
-  (println (nth (list 1 2 3) 0))
-  (println (cons 3 (list 1 2)))
-
-  ; Sets
-  (println (get (set '(1 1 2)) 2))
-  (println (get (set '(3 2)) 2))
-  (println (contains? (set '(1 2 3)) 4))
-  (println (disj (set '(1 2 3)) 3))
-
-  ; Vectors
-  (println (get (vector 3 2 1) 2))
-  (println (pop (vector 1 2 3 4)))
-  (println (subvec (vector 1 2 3 4) 1 3))
-
-  ; Maps
-  (def my-map (hash-map "Name" "Chris" "Age" 26))
-  (println my-map)
-  (println (sorted-map 2 "B" 1 "A" 4 "D" 3 "C"))
-  (println (get my-map "Name")) ; Only returns value
-  (println (apply hash-map (find my-map "Name"))) ; Returns both key and value
-  (println (contains? my-map "Name"))
-  (println (keys my-map))
-  (println (vals my-map))
-  (def my-map2 (merge-with + 
-                       (apply hash-map (find my-map "Name")) 
-                       (apply hash-map (find my-map "Age"))))
-  (println (= my-map my-map2)))
