39ef72f06ad749b0e48954a4b234d430ea2c3a8c
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-22 11:41:33 +0100

    Use True/False Conditionals

--- banas_tutorial/src/tutorial/core.clj
+++ banas_tutorial/src/tutorial/core.clj
@@ -2,38 +2,40 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-(defn say-hello
-  "Receives a name with 1 parameter and responds with 'Hello'"
-  [name]
-  (println "Hello again " name))
+; True, False, and Conditionals
+(defn can-vote
+  [age]
+  (if (>= age 18)
+    (println "You can vote")
+    (println "You can't vote")))
 
-; Adds two numbers together
-(defn get-sum
-  [x y]
-  (println (+ x y)))
+(defn can-do-more
+  [age]
+  (if (>= age 18)
+    (do (println "You can drive")
+        (println "You can vote"))))
 
-; Uses an interface to add either two or three numbers together
-(defn get-sum-more
-  ([x y z]
-   (println (+ x y z)))
-  ([x y]
-   (get-sum x y))) ; `println` is already implemented in get-sum
+(defn when-ex
+  [tof]
+  (when tof
+    (println "1st thing")
+    (println "2nd thing")))
 
-; Creates a "Hello <name>" String
-(defn hello-you
-  [name]
-  (str "Hello " name))
-
-; Says hello to each name in a list
-(defn hello-all
-  [& names]
-  (println (map hello-you names)))
+(defn what-grade
+  [age]
+  (cond
+    (< age 5) (println "Preschool")
+    (= age 5) (println "Kindergarden")
+    (and 
+      (> age 5) 
+      (<= age 18)) (format "Go to grade %d" (- age 5))
+    :else "Go to college"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (say-hello "Chris")
-  (get-sum 4 5)
-  (get-sum-more 3 4 5)
-  (get-sum-more 3 4)
-  (hello-all "Doug" "Mary" "Paul"))
+
+  (can-vote 16)
+  (can-do-more 25)
+  (when-ex true)
+  (what-grade 15))
