7c4dfa5c85280eb64f8cea7de63b3650a3ef134f
cbcoutinho <chrisbcoutinho@gmail.com>
2018-02-25 23:56:00 +0100

    Add a {:name _ :email _} validation macro

---
+++ brave-clojure/src/brave_clojure/brave_true_ale.clj
@@ -0,0 +1,59 @@
+(ns brave-clojure.brave-true-ale)
+
+(def order-details
+  {:name "Mitchard Blimmons"
+   :email "mitchard.blimmonsgmail.com"})
+
+(def order-details-validations
+  {:name 
+   ["Please enter a name" not-empty]
+   
+   :email
+   ["Please enter an email address" not-empty
+    
+    "Your email address doesn't look like an email address"
+    #(or (empty? %) (re-seq #"@" %))]})
+
+(defn error-messages-for
+  "Return a seq of error messages"
+  [to-validate message-validator-pairs]
+  (map first (filter #(not ((second %) to-validate))
+                     (partition 2 message-validator-pairs))))
+
+(defn validate
+  "Returns a map with a vector of errors for each key"
+  [to-validate validations]
+  (reduce (fn [errors validation]
+            (let [[fieldname validation-check-groups] validation
+                  value (get to-validate fieldname)
+                  error-messages (error-messages-for value validation-check-groups)]
+              (if (empty? error-messages)
+                errors
+                (assoc errors fieldname error-messages))))
+          {}
+          validations))
+
+(validate order-details order-details-validations)
+; => {:email ("Your email address doesn't look like an email address")}
+
+; Simple implementation of a validation form
+(let [errors (validate order-details order-details-validations)]
+  (if (empty? errors)
+    (println :success)
+    (println :failure errors)))
+
+(defmacro if-valid
+  "Handle validation more concisely"
+  [to-validate validations errors-name & then-else]
+  `(let [~errors-name (validate ~to-validate ~validations)]
+     (if (empty? ~errors-name)
+       ~@then-else)))
+
+(macroexpand
+  '(if-valid order-details order-details-validations my-error-name
+             (println :success)
+             (println :failure my-error-name)))
+; => (let* [my-error-name (brave-clojure.brave-true-ale/validate order-details order-details-validations)]
+;      (if (clojure.core/empty? my-error-name) 
+;        (println :success) 
+;        (println :failure my-error-name)))
